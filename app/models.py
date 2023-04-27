e52b8cbc9ae3813af2930e3395921072df92cd8e
Marvin <marvin.xin@sjsu.edu>
2023-04-27 00:25:36 -0700

    Fixed ToDo class

--- app/models.py
+++ app/models.py
@@ -41,10 +41,9 @@ def load_user(id):
 
 
 class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(1000), nullable=False)
+    done = db.Column(db.Boolean, default=False)
 
-        id = db.Column(db.Integer, primary_key=True)
-        description = db.Column(db.String(1000), nullable=False)
-        done = db.Column(db.Boolean, default=False)
-
-        user = db.Colum(db.Integer, db.ForeignKey('user.id'))
+    user = db.Column(db.Integer, db.ForeignKey('user.id'))
         
